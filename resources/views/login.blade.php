099e5e3c762e30c0c9a09cd74c34bbbc7fede24f
esraa <esraafahmy03@gmail.com>
2021-08-31 22:49:47 +0200

    add registeration page

---
+++ resources/views/login.blade.php
@@ -0,0 +1,78 @@
+@include('shared.header');
+@include('shared.nav');
+@include('shared.sidNav');
+<section class="content">
+    <div class="container-fluid">
+        <div class="row clearfix">
+        <body class="login-page">
+    <div class="login-box">
+        <div class="logo">
+            <a href="javascript:void(0);"><b>Bit</b> by <b>Bit</b></a>
+            <small>E-Learning</small>
+        </div>
+        <div class="card">
+            <div class="body">
+                <form id="sign_in" method="POST" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" enctype="multipart/form-data">
+                    <div class="msg">Sign in to start your session</div>
+                    <div class="input-group">
+                        <span class="input-group-addon">
+                            <i class="material-icons">email</i>
+                        </span>
+                        <div class="form-line">
+                            <input type="text" class="form-control" name="email" placeholder="email" autofocus>
+                        </div>
+                    </div>
+                    <div class="input-group">
+                        <span class="input-group-addon">
+                            <i class="material-icons">lock</i>
+                        </span>
+                        <div class="form-line">
+                            <input type="password" class="form-control" name="password" placeholder="Password">
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-xs-8 p-t-5">
+                            <!-- <input type="checkbox" name="rememberme" id="rememberme" class="filled-in chk-col-pink">
+                            <label for="rememberme">Remember Me</label> -->
+                        </div>
+                        <div class="col-xs-4">
+                            <button class="btn btn-success  waves-effect" type="submit">SIGN IN</button>
+                        </div>
+                    </div>
+                    <div class="row m-t-15 m-b--20">
+                        <div class="col-xs-6">
+                            <a href="register.php">Register Now!</a>
+                        </div>
+                        <!-- <div class="col-xs-6 align-right">
+                            <a href="forgot-password.html">Forgot Password?</a>
+                        </div> -->
+                    </div>
+                </form>
+
+                <!-- show errors  -->
+                <div class=" m-t-15">
+                    <?php
+                    if (count($errors) > 0) {
+
+
+                        foreach ($errors as $key => $value) {
+                            # code...
+
+                            echo '
+                            <div class="alert alert-danger">
+                            <strong>' . $key . ': </strong> ' . $value .
+                                ' </div>' . '<br>';
+                        }
+                    }
+                    ?>
+                </div>
+
+
+            </div>
+        </div>
+    </div>
+
+        </div>
+    </div>
+</section>
+@include('shared.footer');
